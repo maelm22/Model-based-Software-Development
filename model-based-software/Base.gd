e3804e5c2a2fbbade36b018ea0d3178fde2e6ce2
maelm22 <110687539+maelm22@users.noreply.github.com>
2026-02-25 12:40:06 +0100

    Base State

--- model-based-software/Base.gd
+++ model-based-software/Base.gd
@@ -1,11 +1,17 @@
 extends Node
 
+var baseMachine: StateMachine
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	baseMachine.new()\
+	.AddState("Guard", StateMachine.new()\
+		.AddState("Patrol", Patrol.new())\
+		.AddState("Idle", Idle.new())
+	\
+	)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	baseMachine.Maintain()
